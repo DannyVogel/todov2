d3e3e29797bc7ec0303a81120af15bd20aaa50a4
dvogelg4al <134928350+dvogelg4al@users.noreply.github.com>
2023-12-24 09:33:56 +0100

    update favicon

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- <a href="https://www.flaticon.com/free-icons/work-order" title="work-order icons">Work-order icons created by Muhammad Atif - Flaticon</a> -->
   <NuxtLayout>
     <!-- Markup shared across all pages, ex: NavBar -->
     <NuxtPage />
