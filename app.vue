21721fcc8710672c65207ef0ed3d77ecb2f47d9f
dvogelg4al <134928350+dvogelg4al@users.noreply.github.com>
2023-12-24 19:35:08 +0100

    feat: add onAuthStateChanged to keep user logged in

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import { onAuthStateChanged, auth, type User } from "~/config/firebase";
+const userStore = useUserStore();
+const toDosStore = useToDosStore();
+const { getToDos } = useDatabase();
+
+onAuthStateChanged(auth, (user: User | null) => {
+  if (user) {
+    const userInfo = {
+      username: user.displayName,
+      uid: user.uid,
+      isLogged: true,
+    };
+    userStore.setLoggedInUser(userInfo);
+    getToDos();
+  } else {
+    userStore.logout();
+    toDosStore.logout();
+  }
+});
+</script>
+
 <template>
   <!-- <a href="https://www.flaticon.com/free-icons/work-order" title="work-order icons">Work-order icons created by Muhammad Atif - Flaticon</a> -->
   <NuxtPwaManifest />
