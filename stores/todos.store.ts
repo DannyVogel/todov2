e043bd05f50a156016299c9cf3c1531c85ab5b10
dvogelg4al <134928350+dvogelg4al@users.noreply.github.com>
2023-12-26 09:39:57 +0100

    fix: add persisted state to pinia

--- stores/todos.store.ts
+++ stores/todos.store.ts
@@ -1,61 +1,65 @@
 import type { ToDo } from "~/types/interfaces";
 import { v4 as uuidv4 } from "uuid";
 
-export const useToDosStore = defineStore("toDos", () => {
-  // const name = ref('Eduardo') // <- this is state
-  // const doubleCount = computed(() => count.value * 2) // <- this is a getter
-  // function increment() { // <- this is an action
-  //   count.value++
-  // }
+export const useToDosStore = defineStore(
+  "toDos",
+  () => {
+    // const name = ref('Eduardo') // <- this is state
+    // const doubleCount = computed(() => count.value * 2) // <- this is a getter
+    // function increment() { // <- this is an action
+    //   count.value++
+    // }
 
-  const toDos = ref<ToDo[]>([]);
+    const toDos = ref<ToDo[]>([]);
 
-  const setUserToDoList = (newTodos: ToDo[]) => {
-    toDos.value = newTodos;
-  };
-
-  const addToDo = (text: string) => {
-    const newToDo: ToDo = {
-      id: uuidv4(),
-      text,
-      checked: false,
+    const setUserToDoList = (newTodos: ToDo[]) => {
+      toDos.value = newTodos;
     };
-    toDos.value.push(newToDo);
-  };
 
-  const removeToDo = (toDoToRemoveId: string) => {
-    toDos.value = toDos.value.filter((toDo) => toDo.id !== toDoToRemoveId);
-  };
+    const addToDo = (text: string) => {
+      const newToDo: ToDo = {
+        id: uuidv4(),
+        text,
+        checked: false,
+      };
+      toDos.value.push(newToDo);
+    };
 
-  const setToDoChecked = (toDoToCheckId: string, checked: boolean) => {
-    const toDoToCheck = toDos.value.find(
-      (toDo) => toDo.id === toDoToCheckId
-    ) as ToDo;
-    toDoToCheck.checked = checked;
-  };
+    const removeToDo = (toDoToRemoveId: string) => {
+      toDos.value = toDos.value.filter((toDo) => toDo.id !== toDoToRemoveId);
+    };
 
-  const logout = () => {
-    toDos.value = [];
-  };
+    const setToDoChecked = (toDoToCheckId: string, checked: boolean) => {
+      const toDoToCheck = toDos.value.find(
+        (toDo) => toDo.id === toDoToCheckId
+      ) as ToDo;
+      toDoToCheck.checked = checked;
+    };
 
-  const { writeToDosToDB } = useDatabase();
-  const userStore = useUserStore();
-  watch(
-    toDos,
-    (newToDos) => {
-      console.log("toDos changed", newToDos);
-      if (!userStore.uid) return;
-      writeToDosToDB(newToDos);
-    },
-    { deep: true }
-  );
+    const logout = () => {
+      toDos.value = [];
+    };
 
-  return {
-    toDos,
-    setUserToDoList,
-    addToDo,
-    removeToDo,
-    setToDoChecked,
-    logout,
-  };
-});
+    const { writeToDosToDB } = useDatabase();
+    const userStore = useUserStore();
+    watch(
+      toDos,
+      (newToDos) => {
+        console.log("toDos changed", newToDos);
+        if (!userStore.uid) return;
+        writeToDosToDB(newToDos);
+      },
+      { deep: true }
+    );
+
+    return {
+      toDos,
+      setUserToDoList,
+      addToDo,
+      removeToDo,
+      setToDoChecked,
+      logout,
+    };
+  },
+  { persist: true }
+);
--- stores/user.store.ts
+++ stores/user.store.ts
@@ -1,21 +1,25 @@
 import { defineStore } from "pinia";
 
-export const useUserStore = defineStore("user", () => {
-  const isLogged = ref(false);
-  const username = ref("");
-  const uid = ref("");
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    const isLogged = ref(false);
+    const username = ref("");
+    const uid = ref("");
 
-  const setLoggedInUser = (user: any) => {
-    username.value = user.username;
-    uid.value = user.uid;
-    isLogged.value = true;
-  };
+    const setLoggedInUser = (user: any) => {
+      username.value = user.username;
+      uid.value = user.uid;
+      isLogged.value = true;
+    };
 
-  const logout = () => {
-    username.value = "";
-    uid.value = "";
-    isLogged.value = false;
-  };
+    const logout = () => {
+      username.value = "";
+      uid.value = "";
+      isLogged.value = false;
+    };
 
-  return { isLogged, username, uid, setLoggedInUser, logout };
-});
+    return { isLogged, username, uid, setLoggedInUser, logout };
+  },
+  { persist: true }
+);
